86b863ce386dc28b707272766e7cf3e35deb4734
jeon jun ik <jji3379@naver.com>
2021-02-02 15:34:06 +0900

    getList, getCount 오버라이드

--- Spring05_Final/src/main/java/com/gura/spring05/cafe/dao/CafeCommentDaoImpl.java
+++ Spring05_Final/src/main/java/com/gura/spring05/cafe/dao/CafeCommentDaoImpl.java
@@ -20,8 +20,8 @@ public class CafeCommentDaoImpl implements CafeCommentDao{
 	}
 	
 	@Override
-	public List<CafeCommentDto> getList(int ref_group) {
-		List<CafeCommentDto> list=session.selectList("cafeComment.getList", ref_group);
+	public List<CafeCommentDto> getList(CafeCommentDto dto) {
+		List<CafeCommentDto> list=session.selectList("cafeComment.getList", dto);
 		return list;
 	}
 
@@ -55,6 +55,12 @@ public class CafeCommentDaoImpl implements CafeCommentDao{
 		 */
 		int seq=session.selectOne("cafeComment.getSequence");
 		return seq;
+	}
+
+	@Override
+	public int getCount(int ref_group) {
+		
+		return session.selectOne("cafeComment.getCount", ref_group);
 	}
 	
 }
